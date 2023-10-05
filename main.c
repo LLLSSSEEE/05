53910bd7070f36a9ccc93e6ffafbf5866c224144
Chamomile <dltldms5271@gmail.com>
2023-10-05 16:21:04 +0900

    03

--- main.c
+++ main.c
@@ -3,23 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    int a;
-    printf("input a integer : ");
-    scanf("%d", &a);
+    int num = 0; // 괜히 이상한 숫자 들어가 있으면 안되니 까 
+    char c;
+    printf("input a string : ");
+    //scanf("%c", &a); getchar 쓰면 이거 안써도 됨. 
     
-    if (a>0)
+
+    while ((c=getchar())!='\n')
     {
-         printf("absolute value is %d.\n", a);
+          if((c>='0') && (c<='9'))
+          {
+              num++; // num = num + 1
+          }
     }
-    else if (a<0)
-    {
-         printf("absolute value is %d.\n", -a);
-    }
-    else
-    {
-         printf("absolute value is %d.\n", a);
-    } 
-       
+              
+          
+    printf("the number of digits is %i\n.", num); 
   
   system("PAUSE");	
   return 0;
